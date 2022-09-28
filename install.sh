8e005c8f40e15d05034c63f2e0fab184f2a59433
toddtee <todd@toddtee.sh>
2022-09-28 16:00:45 +1000

    🚀Add Install Script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export GITHUB_USER=toddtee
+export BREWFILE=
+
+#Install xcode
+/usr/bin/xcode-select --install
+
+echo "After Xcode Command Line Developer Tools has installed press enter."
+read
+
+# Update MacOS
+softwareupdate -i -a
+
+# Install chezmoi
+which chezmoi || sh -c "$(curl -fsLS https://chezmoi.io/get)"
+
+# Setup chezmoi
+$HOME/bin/chezmoi init $GITHUB_USER --ssh
+
+# Install brew if not present
+which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Change into chezmoi directory
+chezmoi cd
+
+#Start brewing...
+brew update
+brew upgrade
+brew tap homebrew/bundle
+brew bundle --verbose
